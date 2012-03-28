2c6a20fac6a1377872c1b0faa8fce15d49381513
Vladimir Ovcharenko <ovcharenkovv@gmail.com>
2012-03-28 14:52:16 +0300

    setup rails 3.2.2

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lapasta::Application.config.secret_token = 'cc142b419875797a8889e7348aa1ceb647f47fd64d36b2709f7ede0b393d2c930e1e9f31f002dfe21cd989dacdb61c000da180cafa3bf957750af3e7924c23d6'
