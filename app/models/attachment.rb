a8cc1561dd6b7b37e47d19a72f4e2870b00a650a
Ovcharenko Vladimir <ovcharenkovv@gmail.com>
2012-04-04 14:12:33 +0300

    revert to 75 width small image

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -4,7 +4,7 @@ class Attachment < ActiveRecord::Base
   has_attached_file :attachment,
                     :url  => "/system/attachments/:id/:style_:basename.:extension",
                     :path => ":rails_root/public/system/attachments/:id/:style_:basename.:extension",
-	                  :styles => { :thumb=> "80x80#", :catalog=>"180x180#",:on_show  => "402x402#", :large => "1000x1000#" }
+	                  :styles => { :thumb=> "70x70#", :catalog=>"180x180#",:on_show  => "402x402#", :large => "1000x1000#" }
 
   def attachment_url style
     "/system/attachments/#{self.id}/#{style}_#{self.attachment_file_name}"
