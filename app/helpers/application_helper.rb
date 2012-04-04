da69f8c8b72189ece52445aab1e04f54f05b5812
Ovcharenko Vladimir <ovcharenkovv@gmail.com>
2012-04-04 15:00:53 +0300

    add brad cramps links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,26 @@
+# -*- encoding : utf-8 -*-
 module ApplicationHelper
+  def breadcrumb(param)
+    separator = ' » '
+    ret = link_to 'Главна','/'
+
+    case param[:controller]
+      when 'pages'
+        ret += separator
+        ret += link_to PageCategory.find_by_slug(param[:page_category_slug]).name, short_category_page_url(param[:page_category_slug])
+        if param[:action]=='show'
+          ret += separator
+          ret += link_to Page.find_by_slug(param[:page_slug]).title
+        end
+      when 'products'
+        ret += separator
+        ret += link_to Category.find(param[:category_id]).name, category_products_url(param[:category_id])
+        if param[:action]=='show'
+          ret += separator
+          ret += link_to Product.find(param[:id]).title
+        end
+      else
+    end
+    ret
+  end
 end
