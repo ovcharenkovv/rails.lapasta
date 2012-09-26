6bbb04345e63b0f070e454b2a445890e2fff00cd
Ovcharenko Volodymyr <vova.o@masterofcode.com>
2012-09-26 13:15:27 +0300

    small fix for ree

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,7 @@
 # -*- encoding : utf-8 -*-
 # Be sure to restart your server when you modify this file.
 
-Lapasta::Application.config.session_store :cookie_store, key: '_lapasta_session'
+Lapasta::Application.config.session_store :cookie_store, :key => '_lapasta_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
